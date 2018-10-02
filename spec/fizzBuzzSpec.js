42cb2bfd113212d885a2c2827b187a5dc8942e0a
ChloeHelen <cmingay@fastmail.fm>
2018-10-02 14:23:14 +0100

    Returns FizzBuzz for multiples of 3 and 5

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -5,4 +5,7 @@ describe('fizzbuzz', function () {
     it('return "buzz" when passed a multiple of 5', function () {
         expect(fizzbuzz(5)).toEqual('Buzz');
     });
+    it('return "FizzBuzz" when passed a multiple of both 3 and 5', function () {
+        expect(fizzbuzz(15)).toEqual('FizzBuzz');
+    });
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,7 +1,9 @@
-fizzbuzz = function (x) {
-    if ( x % 3 == 0 ) {
-      return "Fizz";
+let fizzbuzz = function (x) {
+    if ( x % 3 == 0 && x % 5 == 0 ) {
+        return "FizzBuzz";
+    } else if (x % 3 ==0 ) {
+        return "Fizz";
     } else if (x % 5 == 0) {
-      return "Buzz";
+        return "Buzz";
     };
 };
