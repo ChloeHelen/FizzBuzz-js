50b96e2c7799df703e03a8fbdb1d64847c503a55
ChloeHelen <cmingay@fastmail.fm>
2018-10-02 14:13:24 +0100

    returns buzz when passed multiples of 5

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,5 +1,8 @@
-describe('fizzbuzz multiples of 3', function () {
+describe('fizzbuzz', function () {
     it('return "fizz" when passed a multiple of 3', function () {
         expect(fizzbuzz(3)).toEqual('Fizz');
+    });
+    it('return "buzz" when passed a multiple of 5', function () {
+        expect(fizzbuzz(5)).toEqual('Buzz');
     });
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,4 +1,7 @@
 fizzbuzz = function (x) {
-    if ( x % 3 == 0 );
-    return "Fizz";
+    if ( x % 3 == 0 ) {
+      return "Fizz";
+    } else if (x % 5 == 0) {
+      return "Buzz";
+    };
 };
